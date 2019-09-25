63ffd47ba5b499032d3b09395fc1a5e706f9406c
Thibault Brillet <thbrillet@gmail.com>
2019-09-25 19:22:14 +0200

    fix @recettes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#Recette.destroy_all
+Recette.destroy_all
 
 Recette.create!(
   name: "Gelée nettoyante à la Pomme - 100ml",
