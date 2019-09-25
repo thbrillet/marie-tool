3e082369169aed59b11fbe1cdac11a938d985a80
Thibault Brillet <thbrillet@gmail.com>
2019-09-25 18:52:05 +0200

    fix migration

--- db/migrate/20190815151612_change_zone_usage_recette.rb
+++ db/migrate/20190815151612_change_zone_usage_recette.rb
@@ -2,7 +2,7 @@ class ChangeZoneUsageRecette < ActiveRecord::Migration[5.2]
   def change
     remove_column :recettes, :usage, :string
     remove_column :recettes, :zone, :string
-    add_column :recettes, :usage, :text, default: [].to_yaml, array:true
-    add_column :recettes, :zone, :text, default: [].to_yaml, array:true
+    add_column :recettes, :usage, :text, default: [], array:true
+    add_column :recettes, :zone, :text, default: [], array:true
   end
 end
